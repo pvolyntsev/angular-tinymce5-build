34c4707d260663c92bc3dcf6037d38a0d1ccdcd8
Ziben Konstantin <ziben.konstantin@gmail.com>
2020-04-09 17:32:13 +0300

    Добавлена разметка и стили

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -45,9 +45,25 @@ export class EditorComponent implements OnInit {
   protected createForm() {
     this.form = this.fb.group({
       text: [
-        `<p>Next, use our Get Started
-<gap-selectbox><gap-variant selected="selected">docs</gap-variant><gap-variant>1</gap-variant><gap-variant>2</gap-variant></gap-selectbox>
- to setup Tiny!</p>`
+        `<h1>Заголовок 1</h1>
+	   <p><em>“Знаменитый монолог Гамлета из одноименной пьесы Шекспира начинается со слов «to be or not to be», что в переводе на русский язык означает «быть или не быть». Глагол «to be» - один из самых часто употребляемых глаголов в английском языке. Это глагол состояния, который не выражает никакой конкретной деятельности или действия, а вместо этого описывает существование. Также, он используется в качестве вспомогательного глагола, например, с настоящим длительным“</em></p>
+	   <table style="border-collapse: collapse; width: 100%;" border="1" data-mce-style="border-collapse: collapse; width: 100%;">
+		<tbody>
+			<tr>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Заголовок</td>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Заголовок</td>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Заголовок</td>
+			</tr>
+		  	<tr>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Ячейка</td>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Ячейка</td>
+				<td style="width: 33.3333%;" data-mce-style="width: 33.3333%;">Ячейка</td>
+			</tr>
+	  	</tbody>
+	  </table>
+	  <p>Next, use our Get Started <gap-selectbox><gap-variant selected="selected">docs</gap-variant><gap-variant>1</gap-variant><gap-variant>2</gap-variant></gap-selectbox>
+		  to setup Tiny!
+	  </p>`
       ],
     });
   }
